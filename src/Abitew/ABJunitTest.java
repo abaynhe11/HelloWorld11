a3747ccc0d2c2ce5c70f31eb0504da4828498efa
abaynheabitew <abaynheabitew@yahoo.com>
2018-03-02 13:28:07 -0500

    fixed code

---
+++ src/Abitew/ABJunitTest.java
@@ -0,0 +1,14 @@
+package Abitew;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ABJunitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/Abitew/HelloWorld.java
+++ src/Abitew/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Hello World!!");
-      system.out.println("good is good")
+      
+      System.out.println("good is good");
 	}
 
 }
